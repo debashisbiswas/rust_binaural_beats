f47cbe41dd810789724a95de9df4dc16b17511e3
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2022-06-04 21:00:47 -0700

    Add command-line arguments with clap

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,53 @@ use std::{
     f64::consts::PI,
     fs::File,
     io::{Error, Write},
+    time::Instant,
 };
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Bitrate, must be 8 or 16
+    #[clap(short, long, default_value_t = 16, parse(try_from_str=check_bitrate))]
+    bitrate: u16,
+
+    /// Difference between tones in left/right channels
+    #[clap(short, long, default_value_t = 2.0)]
+    difference: f64,
+
+    /// Number of seconds of audio to generate
+    #[clap(short, long, default_value_t = 30)]
+    length: u32,
+}
 
 const SAMPLE_RATE: u32 = 44100;
 const NUM_CHANNELS: u16 = 2;
 const BITS_PER_SAMPLE: u16 = 16;
+const SUPPORTED_BITRATES: [u16; 2] = [8, 16];
 
 const SUBCHUNK1_SIZE: u32 = 16;
 const AUDIO_FORMAT: u16 = 1; // PCM = 1
 
-fn make_sin_sample(step: f64, freq: f64) -> Result<Vec<u8>, &'static str> {
+fn check_bitrate(s: &str) -> Result<u16, String> {
+    let bitrate: u16 = s
+        .parse()
+        .map_err(|_| format!("`{}` isn't a valid number", s))?;
+    if SUPPORTED_BITRATES.contains(&bitrate) {
+        Ok(bitrate)
+    } else {
+        Err(format!(
+            "Bitrate not in supported bitrates: {:?}",
+            SUPPORTED_BITRATES
+        ))
+    }
+}
+
+fn make_sin_sample(step: f64, freq: f64, bitrate: u16) -> Result<Vec<u8>, &'static str> {
     let mut output = Vec::new();
     let sin = f64::sin(2.0 * PI * freq * step / SAMPLE_RATE as f64);
-    match BITS_PER_SAMPLE {
+    match bitrate {
         8 => {
             let s = f64::floor(255.0 * (0.5 * sin + 0.5)) as u8;
             output.push(s);
@@ -31,14 +65,13 @@ fn make_sin_sample(step: f64, freq: f64) -> Result<Vec<u8>, &'static str> {
     Ok(output)
 }
 
-fn generate_samples(secs: u32, freq: f64) -> Vec<u8> {
-    let num_samples = secs * SAMPLE_RATE * NUM_CHANNELS as u32;
+fn generate_samples(secs: u32, freq: f64, bitrate: u16, diff: f64) -> Vec<u8> {
+    let num_samples = secs * SAMPLE_RATE as u32;
     let mut buffer = Vec::with_capacity(num_samples as usize);
-    let difference: f64 = 1.0;
 
     for t in 0..num_samples {
         let t = t as f64;
-        let left_sample = make_sin_sample(t, freq);
+        let left_sample = make_sin_sample(t, freq, bitrate);
         match left_sample {
             Ok(samples) => buffer.extend(samples),
             Err(e) => {
@@ -48,8 +81,8 @@ fn generate_samples(secs: u32, freq: f64) -> Vec<u8> {
         }
 
         if NUM_CHANNELS == 2 {
-            let freq = freq + difference;
-            let right_sample = make_sin_sample(t, freq);
+            let freq = freq + diff;
+            let right_sample = make_sin_sample(t, freq, bitrate);
             match right_sample {
                 Ok(samples) => buffer.extend(samples),
                 Err(e) => {
@@ -84,8 +117,6 @@ fn generate_header(subchunk2_size: u32) -> Vec<u8> {
     buffer.extend_from_slice(&block_align.to_le_bytes()); // block align
     buffer.extend_from_slice(&BITS_PER_SAMPLE.to_le_bytes()); // bit depth
 
-    // TODO: feels like this should be put somewhere else
-    // Data chunk
     buffer.extend_from_slice(b"data");
     buffer.extend_from_slice(&subchunk2_size.to_le_bytes());
 
@@ -93,11 +124,17 @@ fn generate_header(subchunk2_size: u32) -> Vec<u8> {
 }
 
 fn main() -> Result<(), Error> {
+    let timer = Instant::now();
+    let args = Args::parse();
+
     let mut file = File::create("out.wav")?;
-    let samples = generate_samples(20, 220.0);
+    let samples = generate_samples(args.length, 220.0, args.bitrate, args.difference);
     let header = generate_header(samples.len() as u32);
     file.write_all(&header)?;
     file.write_all(&samples)?;
+
+    let elapsed = timer.elapsed().as_secs_f32();
+    println!("Took {:.3} seconds.", elapsed);
 
     Ok(())
 }
